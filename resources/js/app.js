af081f036f1ec95ec9d8a931e67612f5f29ea378
unknown <joperigo@gmail.com>
2022-04-15 20:18:47 -0300

    Vue route setted. '/' point to products

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ require('./bootstrap');
 // import { create } from "lodash";
 import { createApp } from "vue";
 
+import router from "./router"
+
 import App from "./components/App.vue"
 
-createApp(App).mount("#app")
+createApp(App).use(router).mount("#app")
